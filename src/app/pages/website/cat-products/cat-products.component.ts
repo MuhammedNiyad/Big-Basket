1d6139f1f7fdaac3b885727b10a7866ccd301cbe
MuhammedNiyad <niyadpunniyath007@gmail.com>
2024-11-13 23:40:45 +0530

    first commit with project started and created some components

---
+++ src/app/pages/website/cat-products/cat-products.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cat-products',
+  standalone: true,
+  imports: [],
+  templateUrl: './cat-products.component.html',
+  styleUrl: './cat-products.component.css'
+})
+export class CatProductsComponent {
+
+}
